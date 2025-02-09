f8179114c0cae59aaa57fe7c0b1e5c564df2546e
Alex Shadley <shadleyalex@gmail.com>
2025-02-08 21:41:36 -0800

    website overhaul

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-// pages/index.tsx or app/page.tsx
-import Demo from "./components/Demo";
 import { Form } from "./components/Form";
 import {
   SearchCheck,
@@ -7,17 +5,29 @@ import {
   FileText,
   MessageCircleQuestion,
 } from "lucide-react";
+import Image from "next/image";
+
+import logo from "@/app/icon.png";
+import { NewDemo } from "./components/NewDemo";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-50 text-gray-900">
-      <header className="text-center py-16 bg-gradient-to-r from-blue-600 to-indigo-700 text-white shadow-lg">
-        <h1 className="text-6xl font-bold">civ.ai</h1>
-        <p className="text-2xl mt-3">
-          AI-powered search for municipal planners. Keep your planning
-          department up-to-speed, <em>at warp speed.</em>
-        </p>
+      <div className="max-w-5xl mx-auto px-8 py-4 text-2xl font-bold flex gap-2">
+        <Image src={logo} alt="TurboADU" width={30} height={30} />
+        planweave.ai
+      </div>
+      <header className="py-16 bg-gradient-to-r from-blue-600 to-indigo-700 text-white shadow-lg">
+        <div className="max-w-5xl mx-auto px-8">
+          <h1 className="text-5xl font-bold">
+            AI-powered search for city planners
+          </h1>
+          <div className="text-xl mt-4">
+            All of your department's data, at your fingertips.
+          </div>
+        </div>
       </header>
+      <NewDemo />
       <section className="max-w-5xl mx-auto p-8 grid grid-cols-1 md:grid-cols-2 gap-8">
         <div className="p-6 bg-white shadow-md rounded-xl flex gap-2">
           <div className="mt-1">
@@ -67,13 +77,6 @@ export default function Home() {
             </p>
           </div>
         </div>
-      </section>
-      <section className="max-w-5xl mx-auto p-12 text-center">
-        <p className="text-lg mt-6 text-gray-700">
-          Technical studies, emails with the applicant, internal memos,
-          geospatial data - it's all at your fingertips.
-        </p>
-        <Demo />
       </section>
       <section className="max-w-5xl mx-auto p-12 text-center">
         <h2 className="text-4xl font-semibold">Sign Up for Updates</h2>
