b0f887aa6c98d3cbf952dc38334fd114b49ae06e
Alex Shadley <shadleyalex@gmail.com>
2025-02-25 10:49:41 -0800

    remove openai functionality

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -4,23 +4,25 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+// TODO: add OpenAI key to prod environment
+// const openai = new OpenAI({
+//   apiKey: process.env.OPENAI_API_KEY,
+// });
 
 export async function POST(request: Request) {
   try {
     const { message } = await request.json();
 
-    const response = await openai.chat.completions.create({
-      model: "gpt-4o",
-      messages: [
-        { role: "system", content: "You are a helpful assistant." },
-        { role: "user", content: message },
-      ],
-    });
+    // const response = await openai.chat.completions.create({
+    //   model: "gpt-4o",
+    //   messages: [
+    //     { role: "system", content: "You are a helpful assistant." },
+    //     { role: "user", content: message },
+    //   ],
+    // });
 
-    const responseText = response.choices[0].message.content;
+    // const responseText = response.choices[0].message.content;
+    const responseText = "";
 
     return NextResponse.json(
       { success: true, responseText: responseText },
