71d40f682435c45adb1919df83b52dceb44a74ee
Alex Shadley <shadleyalex@gmail.com>
2025-03-25 16:06:54 -0700

    Add --all-clients flag to download script, move to id-based paging
    Two changes:
    1. Added an --all-clients flag so that we can page through cities all in TS, instead of going to bash
    2. started paging through events by event id instead of 'skip' --  I noticed that the api is giving us back results ordered by event id, which makes this possible. This way we can restart downloads of events, and we can also come back later and get new ones.

--- scripts/DownloadLegistarToRaw.ts
+++ scripts/DownloadLegistarToRaw.ts
@@ -1,32 +1,57 @@
 import { rawEvent } from "@/src/db/schema";
+import { AllLegistarClients } from "@/src/constants";
 import { program } from "commander";
 import { db } from "@/src/db";
+import { max, count, eq } from "drizzle-orm";
+import _ from "lodash";
 
-program.option("-c, --client <client>").option("-t, --table <table>");
+program
+  .option("-c, --client <client>")
+  .option("--all-clients")
+  .option("-t, --table <table>");
 
 program.parse(process.argv);
 const options = program.opts();
 
-const downloadRawEvent = async () => {
+const downloadRawEventsForClient = async (client: string) => {
   const pageSize = 200;
-  let skip = 0;
+
+  const [{ eventCount, maxEventId }] = await db
+    .select({ eventCount: count(), maxEventId: max(rawEvent.legistarEventId) })
+    .from(rawEvent)
+    .where(eq(rawEvent.legistarClient, client))
+    .execute();
+
+  let nextEventId = maxEventId ? maxEventId + 1 : 0;
+  console.log(
+    `Existing events: ${eventCount}\nStarting at event id: ${nextEventId}`
+  );
 
   while (true) {
-    console.log(`Downloading raw_event skip=${skip}`);
-    const url = `https://webapi.legistar.com/v1/${options.client}/events?$skip=${skip}&$top=${pageSize}`;
+    console.log(`Downloading raw_event, nextEventId: ${nextEventId}`);
+    const url = `https://webapi.legistar.com/v1/${client}/events?$top=${pageSize}&$filter=EventId+ge+${nextEventId}`;
     const response = await fetch(url);
+
+    if (response.status >= 400) {
+      console.error(`Failed to download events at ${url}`);
+      console.error(`Status code: ${response.status}`);
+      console.error(await response.text());
+      break;
+    }
+
     const data: any[] = await response.json();
 
     if (data.length === 0) {
       console.log("No more events to download");
       break;
     }
+    console.log(`Got ${data.length} events`);
 
     await db
       .insert(rawEvent)
       .values(
         data.map((event) => ({
-          legistarClient: options.client,
+          legistarClient: client,
           legistarEventId: event.EventId,
           json: event,
         }))
@@ -38,15 +63,22 @@ const downloadRawEvent = async () => {
         },
       });
 
-    skip += pageSize;
+    nextEventId = _.max(data.map((event) => event.EventId)) + 1;
   }
 };
 
 const main = async () => {
-  console.log(`Downloading ${options.table} for ${options.client}`);
+  if (options.allClients) {
+    for (const client of AllLegistarClients) {
+      console.log(`Downloading ${options.table} for ${client}`);
+      await downloadRawEventsForClient(client);
+    }
+  } else {
+    console.log(`Downloading ${options.table} for ${options.client}`);
 
-  if (options.table === "raw_event") {
-    await downloadRawEvent();
+    if (options.table === "raw_event") {
+      await downloadRawEventsForClient(options.client);
+    }
   }
 };
 
