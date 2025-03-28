b7f4710b453852590a79b179a7ff8974f06c2474
Alex Shadley <shadleyalex@gmail.com>
2025-03-27 18:29:14 -0700

    add civic plus to dbt
    had to cast source ids to text because some civic plus ids aren't valid ints. No big deal.

--- dbt/models/staging/stg_unified_document_text.sql
+++ dbt/models/staging/stg_unified_document_text.sql
@@ -7,7 +7,7 @@
 select
   'legistar' as source,
   legistar_client as client,
-  legistar_event_id as source_event_id,
+  cast(legistar_event_id as text) as source_event_id,
   null as source_document_id,
   'legistar' || ':' || legistar_client || ':' || legistar_event_id as unified_event_id,
   substring(text, 1, 5000) as truncated_text
@@ -18,8 +18,19 @@ union all
 select
   'prime_gov' as source,
   prime_gov_client as client,
-  prime_gov_meeting_id as source_event_id,
-  prime_gov_document_id as source_document_id,
+  cast(prime_gov_meeting_id as text) as source_event_id,
+  cast(prime_gov_document_id as text) as source_document_id,
   'prime_gov' || ':' || prime_gov_client || ':' || prime_gov_meeting_id as unified_event_id,
   substring(text, 1, 5000) as truncated_text
 from {{ source('raw', 'prime_gov_document_text') }}
+
+union all
+
+select
+  'civic_plus' as source,
+  city_name as client,
+  civicplus_meeting_id as source_event_id,
+  document_id as source_document_id,
+  'civic_plus' || ':' || city_name || ':' || civicplus_meeting_id as unified_event_id,
+  substring(text, 1, 5000) as truncated_text
+from {{ source('raw', 'civic_plus_document_text') }}
--- dbt/models/staging/stg_unified_event.sql
+++ dbt/models/staging/stg_unified_event.sql
@@ -7,8 +7,8 @@
 select
   'legistar' as source,
   legistar_client as client,
-  legistar_event_id as source_event_id,
-  'legistar' || ':' || legistar_client || ':' || legistar_event_id as unified_event_id,
+  cast(legistar_event_id as text) as source_event_id,
+  'legistar' || ':' || legistar_client || ':' ||legistar_event_id as unified_event_id,
   cast(json->>'EventDate' as date) as event_date,
   json->>'EventBodyName' as event_body_name
 from {{ source('raw', 'raw_event') }}
@@ -18,8 +18,20 @@ union all
 select
   'prime_gov' as source,
   prime_gov_client as client,
-  prime_gov_meeting_id as source_event_id,
+  cast(prime_gov_meeting_id as text) as source_event_id,
   'prime_gov' || ':' || prime_gov_client || ':' || prime_gov_meeting_id as unified_event_id,
   cast(json->>'dateTime' as date) as event_date,
   json->>'title' as event_body_name
 from {{ source('raw', 'raw_prime_gov_meeting') }}
+
+union all
+
+select
+  'civic_plus' as source,
+  city_name as client,
+  civicplus_meeting_id as source_event_id,
+  'civic_plus' || ':' || city_name || ':' || civicplus_meeting_id as unified_event_id,
+  cast(json->>'meeting_date' as date) as event_date,
+  json->>'committee_name' as event_body_name
+from {{ source('raw', 'raw_civicplus_asset') }}
+where asset_type = 'meeting'  -- Only include meeting records, not other asset types
