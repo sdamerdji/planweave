f1a512d0c51fa29b9c6bdc85709dde93eaad7e77
Alex Shadley <shadleyalex@gmail.com>
2025-05-04 17:59:15 -0700

    make moving text not change height on mobile

--- app/components/AnimatedTagline.tsx
+++ app/components/AnimatedTagline.tsx
@@ -5,8 +5,8 @@ import { useState, useEffect } from "react";
 const phrases = [
   "Fewer rounds of reviews",
   "Fewer revisions",
-  "Faster approvals", 
-  "More time for design"
+  "Faster approvals",
+  "More time for design",
 ];
 
 export const AnimatedTagline = () => {
@@ -17,36 +17,33 @@ export const AnimatedTagline = () => {
     const interval = setInterval(() => {
       // Start fade out
       setIsVisible(false);
-      
+
       // Change phrase and fade in after animation completes
       setTimeout(() => {
         setCurrentPhraseIndex((prevIndex) => (prevIndex + 1) % phrases.length);
         setIsVisible(true);
       }, 600); // Transition time
-      
     }, 3000); // Display time
-    
+
     return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="w-full py-10 bg-gray-50">
+    <div className="w-full pt-10 pb-4 bg-gray-50 h-[80px]">
       <div className="max-w-5xl mx-auto px-8 relative">
-        <div 
+        <div
           className={`text-left text-3xl md:text-4xl transition-all duration-600 ${
             isVisible ? "opacity-100 translate-x-0" : "opacity-0 -translate-x-6"
           }`}
           style={{
-            fontFamily: 'lora',
-            letterSpacing: '0.01em'
+            fontFamily: "lora",
+            letterSpacing: "0.01em",
           }}
         >
-          <span className="text-indigo-700">
-            {phrases[currentPhraseIndex]}
-          </span>
+          <span className="text-indigo-700">{phrases[currentPhraseIndex]}</span>
           <span className="inline-block ml-1 text-gray-300 transform -translate-y-1"></span>
         </div>
       </div>
     </div>
   );
-}; 
+};
